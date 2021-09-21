9712b7c2a12013d0a20efa0a381300695cb9d69d
anixudh <anirudh.varma196@gmail.com>
2021-09-21 21:00:17 +0530

    added remove and read buttons

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-function Book(name, author, nop, read){
+function Book(name, author, nop, read,index){
     this.name=name;
     this.author=author;
     this.nop=nop;
     this.read=read;
+    this.index=index;
 };
 
 const newBook=e=>{
@@ -16,12 +17,24 @@ const submitBook=e=>{
     let aname=document.querySelector("#author-name");
     let nop=document.querySelector("#pages");
     let read=document.querySelector("#read");
-    console.log(bname.value, aname.value, nop.value, read.checked);
-    let book=new Book(bname.value, aname.value, nop.value, read.checked);
+    /*console.log(bname.value, aname.value, nop.value, read.checked);*/
+    let book=new Book(bname.value, aname.value, nop.value, read.checked, i);
     createNewBook(book);
 };
 
 createNewBook=(book)=>{
+
+    const changeRead=e=>{
+        book.read=!book.read;
+        read.textContent=`Read status: ${(book.read)?`Yes`:`No`}`;
+    };
+
+    const removeBook=e=>{
+        cards.removeChild(card);
+        bookArr.splice(i--,1);
+    };
+
+    bookArr[i++]=book;
     let card=document.createElement("div");
     let bname=document.createElement("p");
     bname.textContent=`Book Name: ${book.name}`;
@@ -31,12 +44,28 @@ createNewBook=(book)=>{
     nop.textContent=`No. of Pages: ${book.nop}`;
     let read=document.createElement("p");
     read.textContent=`Read status: ${(book.read)?`Yes`:`No`}`;
+
+   /* let buttons=document.createElement("div"); */
+    let readButton=document.createElement("button");
+    let remButton=document.createElement("button");
+    readButton.textContent="READ";
+    remButton.textContent="X";
+    readButton.classList.add("readButton");
+    remButton.classList.add("remButton");
+
+    card.appendChild(remButton);
     card.appendChild(bname);
     card.appendChild(aname);
     card.appendChild(nop);
     card.appendChild(read);
+    card.appendChild(readButton);
     card.classList.add("card");
     cards.prepend(card);
+    /*console.log(bookArr,i);*/
+
+    readButton.addEventListener('click',changeRead);
+    remButton.addEventListener('click',removeBook);
+   
 };
 
 const form=document.querySelector("form");
@@ -46,3 +75,6 @@ newB.addEventListener('click',newBook);
 
 const submit=document.querySelector('#submit');
 submit.addEventListener('click',submitBook);
+
+let bookArr=[];
+let i=0;
